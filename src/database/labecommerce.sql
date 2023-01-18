bf0e533241467d33290ffaaec30a156101e19ffd
Luawyn <luanaftrevizani@gmail.com>
2023-01-18 19:04:11 -0300

    finalizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -82,8 +82,25 @@ FOREIGN KEY (buyer_id) REFERENCES users(id)
 
 INSERT INTO purchases (id, total_price, paid, delivered_at, buyer_id)
 VALUES
-("p001", 120.99, 1, DATE('now'), "1"),
-("p002", 1200.50, 0, DATE('now'), "1"),
-("p003", 450.20, 1, DATE('now'), "2"),
-("p004", 150.99, 1, DATE('now'), "2"),
-("p005", 45.99, 0, DATE('now'), "3"),
+("p001", 120.99, 1, '2023-01-10', "1"),
+("p002", 1200.50, 0, '2023-01-10', "1"),
+("p003", 450.20, 1, '2023-01-11', "2"),
+("p004", 150.99, 1, '2023-01-13', "2"),
+("p005", 45.99, 0, '2023-01-12' , "3");
+
+INSERT INTO purchases (id, total_price, paid, delivered_at, buyer_id)
+VALUES
+("p006", 229.99, 1, DATETIME(), "1"),
+("p007", 539.99, 0, DATETIME(), "3"),
+("p008", 129.99, 0, DATETIME(), "2"),
+("p009", 329.99, 0, DATETIME(), "1");
+
+UPDATE purchases
+SET delivered_at = DATETIME()
+WHERE id = "p002";
+
+SELECT * FROM users
+INNER JOIN purchases
+ON purchases.buyer_id = users.id
+-- ORDER BY users.id ASC
+WHERE users.id = "1"
